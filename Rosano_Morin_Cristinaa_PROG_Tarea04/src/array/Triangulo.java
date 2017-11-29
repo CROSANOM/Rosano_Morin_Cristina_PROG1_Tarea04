76126b581a23b8086c84eb4a2bfb76b8f5027b5c
crosanom <crosanom@192.168.1.43>
2017-11-29 22:06:58 +0100

    Commit TAREA3: Creada la clase Triangulo con sus tres atributos como privados

---
+++ Rosano_Morin_Cristinaa_PROG_Tarea04/src/array/Triangulo.java
@@ -0,0 +1,29 @@
+package array;
+
+/**
+ * 
+ * @author crosanom
+ *
+ */
+
+public class Triangulo {
+	/**
+	 * atributos de la clase Triangulo
+	 */
+	private Triangulo verticeA;
+	private Triangulo verticeB;
+	private Triangulo verticeC;
+
+	// constructor por defecto 
+	public Triangulo() {
+		// TODO Auto-generated constructor stub
+
+	
+	
+	
+	
+	
+	
+	}
+
+}// f class

4eaa683e80e9551f56dc03ae8dae3858a4ba027c
crosanom <crosanom@192.168.1.43>
2017-11-29 17:53:58 +0100

    Commit Ejercicio 3 : Se crea un paquete denominado array que por ahora contiene la clase Punto con dos atributos doubles declarados como privados, solo se puede acceder a los mismo a traves de los métodos de acceso setters & getters

--- Rosano_Morin_Cristinaa_PROG_Tarea04/src/array/Punto.java
+++ Rosano_Morin_Cristinaa_PROG_Tarea04/src/array/Punto.java
@@ -4,16 +4,34 @@
 package array;
 
 /**
- * @author crosanom
- *
+ * @author crosanom version1.1
+ */
+/*
+ * Ejericio 3: Se pretende utilizar el diagrama de clases anterior para crear un programa
+ * que nos pida por teclado el número de triángulos a generar. Cree un array de
+ * triángulos del tamaño anteriormente introducido y cree cada triángulo con
+ * vértices generados aleatoriamente entre 0 y 100 (Math.random()*100). Una vez
+ * relleno el array lo recorrerá para calcular la media de los perímetros de los
+ * triángulos e informará del perímetro medio. Una vez hecho esto, volverá a
+ * recorrer el array para contar cuántos triángulos tienen un perímetro mayor o
+ * igual que el perímetro medio y cuántos menor. También deberá quedarse con el
+ * triángulo de mayor perímetro y de menor perímetro e informar de todo ello al
+ * usuario.
+ * 
  */
 public class Punto {
 
 	/**
-	 * 
+	 * atributos de la clase declaran privados 
 	 */
+	private double x;
+	private double y;
+	
+	
+	
+	// constructor por defecto 
 	public Punto() {
 		// TODO Auto-generated constructor stub
 	}
 
-}
+}// f class

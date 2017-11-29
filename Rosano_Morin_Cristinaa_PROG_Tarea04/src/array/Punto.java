f2f601d35cca99fafed135ca842a081f648fc845
crosanom <crosanom@192.168.1.43>
2017-11-29 18:12:39 +0100

    Commit Tarea 3: Se incluyen los métodos getters para los atributos de esta manera podremos acceder a los mismos.

--- Rosano_Morin_Cristinaa_PROG_Tarea04/src/array/Punto.java
+++ Rosano_Morin_Cristinaa_PROG_Tarea04/src/array/Punto.java
@@ -7,46 +7,60 @@ package array;
  * @author crosanom version1.1
  */
 /*
- * Ejericio 3: Se pretende utilizar el diagrama de clases anterior para crear un programa
- * que nos pida por teclado el número de triángulos a generar. Cree un array de
- * triángulos del tamaño anteriormente introducido y cree cada triángulo con
- * vértices generados aleatoriamente entre 0 y 100 (Math.random()*100). Una vez
- * relleno el array lo recorrerá para calcular la media de los perímetros de los
- * triángulos e informará del perímetro medio. Una vez hecho esto, volverá a
- * recorrer el array para contar cuántos triángulos tienen un perímetro mayor o
- * igual que el perímetro medio y cuántos menor. También deberá quedarse con el
- * triángulo de mayor perímetro y de menor perímetro e informar de todo ello al
- * usuario.
+ * Ejericio 3: Se pretende utilizar el diagrama de clases anterior para crear un
+ * programa que nos pida por teclado el número de triángulos a generar. Cree un
+ * array de triángulos del tamaño anteriormente introducido y cree cada
+ * triángulo con vértices generados aleatoriamente entre 0 y 100
+ * (Math.random()*100). Una vez relleno el array lo recorrerá para calcular la
+ * media de los perímetros de los triángulos e informará del perímetro medio.
+ * Una vez hecho esto, volverá a recorrer el array para contar cuántos
+ * triángulos tienen un perímetro mayor o igual que el perímetro medio y cuántos
+ * menor. También deberá quedarse con el triángulo de mayor perímetro y de menor
+ * perímetro e informar de todo ello al usuario.
  * 
  */
 public class Punto {
 
 	/**
-	 * atributos de la clase declaran privados 
+	 * atributos de la clase declaran privados
 	 */
 	private double x;
 	private double y;
-	
-	// constructor por defecto 
+
+	// constructor por defecto
 	public Punto() {
-		// TODO Auto-generated constructor stub
+
 	}
 
-	/** constructor con parametros x y 
+	/**
+	 * constructor con parametros x y
 	 * 
 	 * @param x
 	 * @param y
 	 */
 	public Punto(double x, double y) {
-		
-		this.x=x;
-		this.y=y;	
-		
+
+		this.x = x;
+		this.y = y;
+
 	}
-	
-	// 
-	 
-	
-	
-	
+
+	/*
+	 * se crean los metodos get para poder acceder a los atributos
+	 */
+
+	/**
+	 * @return the x
+	 */
+	protected double getX() {
+		return x;
+	}
+
+	/**
+	 * @return the y
+	 */
+	protected double getY() {
+		return y;
+	}
+
 }// f class

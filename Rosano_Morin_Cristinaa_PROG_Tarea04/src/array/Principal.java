7d49c5273e84f395466bb25d488c57a95e3a30fa
crosanom <crosanom@192.168.1.43>
2017-11-29 23:35:14 +0100

    Commit TAREA3: se incorpora  un mensaje que solicita al usuario que introduzca el numero de triangulos.
    Se define una variable NumTotalPuntos, para ello se requiere de una
    constante que es NPUNTOS_TRIANGULO=3 que se multiplica por el
    numTriangulo ( introducida por teclado) 
    
    Se declara e inicializa un array de puntosTotales

--- Rosano_Morin_Cristinaa_PROG_Tarea04/src/array/Principal.java
+++ Rosano_Morin_Cristinaa_PROG_Tarea04/src/array/Principal.java
@@ -3,6 +3,8 @@
  */
 package array;
 
+import utilidades.Entrada;
+
 /**
  * @author crosanom
  *
@@ -10,16 +12,27 @@ package array;
 public class Principal {
 
 	/**
-	 * 
-	 */
-	
-
-	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
+		// solicitamos por teclado del número de triangulos
+		System.out.println("Introduce el número de triangulos que necesitas:");
+		int numTriangulos = Entrada.entero();
+
+		// se calculan los puntos totales que vamos a necesitar, para ello
+		// tenemos en cuenta que un triangulo siempre tiene 3 puntos
+		// para saber el numero total se multiplica numTriangulos * 3
+
+		int NUMPUNTOS_TRIANGULO = 3; // es una constante porque siempre es 3
+		int puntosTotales = numTriangulos * NUMPUNTOS_TRIANGULO;
+
+		// declaramos e incializamos el array de puntos
+
+		Punto miArrayPuntos[] = new Punto[puntosTotales];
+
+	}// f main
+
+	// TODO Auto-generated method stub
 
 }

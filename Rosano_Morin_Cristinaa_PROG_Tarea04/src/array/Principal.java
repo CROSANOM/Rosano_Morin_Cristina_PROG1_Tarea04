b573c8416800f2b37d91901835d23b79bd670f45
crosanom <crosanom@192.168.1.43>
2017-11-30 01:22:14 +0100

    Commit TAREA3: Se añade al proyecto un metodo for que permite recorrer el array y rellenarlo en cada posición con tres puntos aleatorios, generados con el metodo random de la libreria Math de la clase Math

--- Rosano_Morin_Cristinaa_PROG_Tarea04/src/array/Principal.java
+++ Rosano_Morin_Cristinaa_PROG_Tarea04/src/array/Principal.java
@@ -24,12 +24,45 @@ public class Principal {
 		// tenemos en cuenta que un triangulo siempre tiene 3 puntos
 		// para saber el numero total se multiplica numTriangulos * 3
 
-		int NUMPUNTOS_TRIANGULO = 3; // es una constante porque siempre es 3
-		int puntosTotales = numTriangulos * NUMPUNTOS_TRIANGULO;
+		int NUM_PUNTOS_TRIANGULO = 3; // es una constante porque siempre es 3
+		int puntosTotales = numTriangulos * NUM_PUNTOS_TRIANGULO;
+
+		double xAleatorio1 = 0;
+		double yAleatorio1 = 0;
+		double xAleatorio2 = 0;
+		double yAleatorio2 = 0;
+		double xAleatorio3 = 0;
+		double yAleatorio3 = 0;
 
 		// declaramos e incializamos el array de puntos
 
 		Punto miArrayPuntos[] = new Punto[puntosTotales];
+
+		// generamos los puntos aleatorios
+
+		for (int i = 0; i < miArrayPuntos.length; i++) {
+
+			// rellenamos array con objetos punto aleatorios //
+
+			xAleatorio1 = Math.random() * 100;
+			yAleatorio1 = Math.random() * 100;
+
+			xAleatorio2 = Math.random() * 100;
+			yAleatorio2 = Math.random() * 100;
+
+			xAleatorio3 = Math.random() * 100;
+			yAleatorio3 = Math.random() * 100;
+ 
+			// se instancian tres objetos de la clase Punto que generan los puntos de manera aleatoria
+			
+			Punto miPuntoAleatorio1 = new Punto(xAleatorio1, yAleatorio1);
+			Punto miPuntoAleatorio2 = new Punto(xAleatorio2, yAleatorio2);
+			Punto miPuntoAleatorio3 = new Punto(xAleatorio3, yAleatorio3);
+			
+			// Genera un array de triangulos 
+			
+			
+		}
 
 	}// f main
 
